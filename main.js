12d3794ef30f5c4785978758d3b30e28f70b1f35
hennyguimaraes-hub <henny.guimaraes@escola.pr.gov.br>
2026-05-07 16:34:25 -0300

    .

---
+++ main.js
@@ -0,0 +1,21 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-12-31T00:00:00");
+const tempoObjetivo2 = new Date("2026-12-31T00:00:00");
+const tempoObjetivo3 = new Date("2026-10-31T00:00:00");
+const tempoObjetivo4 = new Date("2026-12-31T00:00:00");
